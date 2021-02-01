75781f5339acde9351c2b27d9b7f9b623edf67d2
Rayhan <rayhancse08@gmail.com>
2021-02-01 12:32:49 +0600

    add rest framework authtoken

--- arot/settings/components/application.py
+++ arot/settings/components/application.py
@@ -5,6 +5,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'api',
 ]
 
